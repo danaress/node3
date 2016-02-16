11dacf6611169a79c27e5d31066f75fdf2a97128
Student <ressdana@gmail.com>
2016-02-15 17:29:46 -0700

    nothing new

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var request = require('request')
+var _ = require('request')
 
 request('https://cdn.rawgit.com/metaraine/swatch/74580660c9229541622bbf1fd4198618d9f677e5/webcolors.json', function (error, response, body) {
 if (!error && response.statusCode == 200) {
